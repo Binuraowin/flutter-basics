e5a4b4da50f1be73cb9bbf5a70f64292e50a52de
binura-owin <binura.owin1@gmail.com>
2023-03-11 23:11:38 +0530

    basic ui components completed

---
+++ flutter_basics/lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const KOutputTextColor = Color(0xFFFF5555);
+const KInputTextColor = Color(0xFFFF8888);
+const KInputLableTextStyle = TextStyle(
+  color: Color(0xFFFF8888),
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+);
+const KOutputTextStyle = TextStyle(
+  color: KOutputTextColor,
+  fontSize: 60,
+  fontWeight: FontWeight.bold,
+);
--- flutter_basics/lib/main_page.dart
+++ flutter_basics/lib/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_basics/constants.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -12,8 +13,132 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.cyan,
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: const [
+                        Icon(
+                          Icons.male,
+                          size: 150,
+                        ),
+                        Text("Male"),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: const [
+                        Icon(
+                          Icons.female,
+                          size: 150,
+                        ),
+                        Text("Female"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        const Text("Height"),
+                        const Text(
+                          "176",
+                          style: KInputLableTextStyle,
+                        ),
+                        Row(
+                          children: const [
+                            FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(
+                                Icons.remove,
+                                size: 40,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(
+                                Icons.add,
+                                size: 40,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        const Text("Weight"),
+                        const Text(
+                          "76",
+                          style: KInputLableTextStyle,
+                        ),
+                        Row(
+                          children: const [
+                            FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(
+                                Icons.remove,
+                                size: 40,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(
+                                Icons.add,
+                                size: 40,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Column(
+                children: const [
+                  Text("BMI"),
+                  Text(
+                    "22.00",
+                    style: KOutputTextStyle,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
